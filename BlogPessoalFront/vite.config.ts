9a1160a6f1ff107f3be54101b8bd4736def20387
Caio Henrique <127551165+HenriqueSam45Br@users.noreply.github.com>
2023-06-02 00:07:15 -0300

    Delete BlogPessoalFront directory

--- BlogPessoalFront/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// clona não q é feio... 🤣
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
